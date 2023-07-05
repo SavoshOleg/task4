413264a3ec7704f93f2793f6bbbbb034c05f0c08
SavoshOleg <138625982+SavoshOleg@users.noreply.github.com>
2023-07-05 23:31:36 +0300

    Add files via upload

---
+++ task4/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h> // Для української мови
+
+int main() {
+system("chcp 1251"); // Для української мови
+system("cls"); // Для української мови
+    unsigned int n;
+    printf("Введіть натуральне число n: ");
+    scanf("%u", &n);
+
+    int count = 0; // використовую змінну count для підрахунку кількості рівних дільників
+    for (unsigned int m = 1; m <= n; m++) {
+        if (n % m == n / m) {
+            count++; // Якщо умова виконується, збільшуємо лічильник count.
+        }
+    }
+
+    printf("Кількість рівних дільників числа %u: %d\n", n, count);
+
+    return 0;
+}
